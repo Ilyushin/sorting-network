80c5253460ff359e8349ec85567d65c0f282d5dd
Ilyushin Evgeniy <john.ilyushin@gmail.com>
2017-12-07 17:18:14 +0300

    Before replacing a class 'Point' to a struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,66 @@
 #include <iostream>
 #include <sortingNetwork.h>
+#include <mpi.h>
 #include "point.h"
 
 typedef std::vector<Point> point_vec_t;
 
+// Iterate since 0 to n1, 1 to n2 and create Points of a grid
 void fillCoord(point_vec_t &points, const int *n1, const int *n2);
 
+// Returns a next coordinate which was calculated using a function:
+//      coord = (counter - 1)*delta
 float getNextCoordinate(int counter, int delta);
+
 
 int main(int argc, char *argv[]) {
 
-    int n1 = 0;
-    int n2 = 0;
+    //PARALLEL AREA
 
-    if (argc >= 3) {
-        n1 = atoi(argv[1]);
-        n2 = atoi(argv[2]);
-    } else {
-        std::cout << "A dimension of a network (n1, n2) wasn't passed to the program!";
-        return 0;
+    MPI_Init(&argc, &argv);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int processors;
+    MPI_Comm_size(MPI_COMM_WORLD, &processors);
+
+    if(rank == 0) {
+        int n1 = 0;
+        int n2 = 0;
+
+        if (argc >= 3) {
+            n1 = atoi(argv[1]);
+            n2 = atoi(argv[2]);
+        } else {
+            std::cout << "A dimension of a network (n1, n2) wasn't passed to the program!\n";
+            return 0;
+        }
+
+        if (n1 > INT_MAX / n2) {
+            std::cout << "A dimension of a network (n1, n2) is too big for calculating!\n";
+            MPI_Finalize();
+            return 0;
+        }
+
+        int size = n1 * n2;
+        // Check the size is even or not
+        // If it's not, creating a dummy point
+        bool needDummy = false;
+        if(size % 2 != 0){
+            needDummy = true;
+        }
+
+        SortingNetwork network = SortingNetwork(size+1);
+        network.buildSchedule();
+
+        point_vec_t points;
+        fillCoord(points, &n1, &n2);
+
+
+
     }
 
-    if (n1 > INT_MAX / n2) {
-        std::cout << "A dimension of a network (n1, n2) is too big for calculating!";
-        return 0;
-    }
-
-    int size = n1 * n2;
-
-    SortingNetwork network = SortingNetwork(size);
-    network.buildSchedule();
-
-    point_vec_t points;
-    fillCoord(points, &n1, &n2);
+    MPI_Finalize();
 
     return 0;
 }
